aca6a78426fad95ec4640d8e209716c711498126
Varun Pravesh <131331265+varunpravesh@users.noreply.github.com>
2025-05-31 03:38:32 +0530

    Delete Section1 directory

--- Section1/Question1.sql
+++
@@ -1,7 +0,0 @@
--- SQL Query for Question1 in Section1
-
-SELECT * 
-FROM table_name;
-
--- Original Answer:
-Answer1
--- Section1/Question2.sql
+++
@@ -1,8 +0,0 @@
--- SQL Query for Question2 in Section1
-
-SELECT column1, column2
-FROM table_name
-WHERE condition;
-
--- Original Answer:
-Answer2
