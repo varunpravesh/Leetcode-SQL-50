75e5ddb531f4f42c2fd91c04ca1c19c79399e4d0
Varun Pravesh <131331265+varunpravesh@users.noreply.github.com>
2025-05-31 23:30:14 +0530

    Added Average Time of Process per Machine SQL file in Basic Joins folder.

---
+++ Basic Joins/Average Time of Process per Machine.sql
@@ -0,0 +1,7 @@
+SELECT A.machine_id,
+       ROUND(AVG(B.timestamp - A.timestamp), 3) AS processing_time
+FROM Activity A
+JOIN Activity B ON A.machine_id = B.machine_id
+WHERE A.process_id = B.process_id
+  AND (A.activity_type = "start" AND B.activity_type = "end")
+GROUP BY A.machine_id;
