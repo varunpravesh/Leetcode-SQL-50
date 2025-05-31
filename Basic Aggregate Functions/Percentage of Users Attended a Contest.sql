c6e55dbc684800b834ef4e03ccd35abb5f1297d8
Varun Pravesh <131331265+varunpravesh@users.noreply.github.com>
2025-05-31 23:31:29 +0530

    Add SQL file for 'Percentage of Users Attended a Contest' in 'Basic Aggregate Functions' section

---
+++ Basic Aggregate Functions/Percentage of Users Attended a Contest.sql
@@ -0,0 +1,13 @@
+SELECT 
+    contest_id, 
+    ROUND(
+        (COUNT(DISTINCT user_id)) / (SELECT COUNT(*) FROM Users) * 100, 
+        2
+    ) AS percentage
+FROM 
+    Register
+GROUP BY 
+    contest_id
+ORDER BY 
+    percentage DESC, 
+    contest_id;
